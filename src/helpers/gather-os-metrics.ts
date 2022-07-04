fc85e3e854cc4bb7941064988ba5bb163abe7c9a
Cal <callum.bass@checkfer.com>
2022-07-04 20:16:03 +0100

    fix: reduce payload size on metric request

--- src/helpers/gather-os-metrics.ts
+++ src/helpers/gather-os-metrics.ts
@@ -4,6 +4,8 @@ import { sendMetrics } from './send-metrics';
 
 export const gatherOsMetrics = async (io: any, span: any, service: any) => {
   try {
+  span.os = span.os.length > 50 ? span.os.splice(-(span.os.length - 50)) : span.os
+  span.responses = span.responses.length > 100 ? span.responses.splice(-(span.responses.length - 100)) : span.responses
   const collectedMetrics = await requestMetrics(service.host, span)
   sendMetrics(io, collectedMetrics.data, service)
   } catch (err) {}
